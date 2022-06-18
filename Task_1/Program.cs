b861adb0c8a5113647e5a45ddcbb51a52e29460d
KseniaKolpashchikova <KseniaKolpashchikova@yandex.ru>
2022-06-18 19:01:46 +0300

    домашняя работа, семинар 2

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите трехзначное число N = ");
+int N = Convert.ToInt32 (Console.ReadLine());
+
+if ((Math.Abs(N) < 100) || (Math.Abs(N) > 999)) {
+    Console.WriteLine("Обратите внимание! Введено не трехзначное число!");
+    return;
+}
+int N1 = ( N % 100) / 10;
+
+WriteLine($"Вторая цифра заданного числа = {N1}");
---
+++ Task_2_/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("Третья цифра заданного числа = " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("В Вашем числе нет третьей цифры");
+}
